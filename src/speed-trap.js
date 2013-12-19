6223762515a4d950ce335dcf843ce36b6eef250c
Shane Tomlinson <stomlinson@mozilla.com>
2013-12-19 11:13:56 +0000

    Cleanup, add PersistentStorage, separate NavigationTiming into its own object. Generate guids.

--- src/speed-trap.js
+++ src/speed-trap.js
@@ -2,51 +2,97 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function(exports) {
-  "use strict";
+(function (exports, undefined) {
+  'use strict';
 
-  var navigationTiming;
-  try {
-    navigationTiming = window.performance.timing;
-  } catch (e) {
-    navigationTiming = {};
-  }
+  var LOCAL_STORAGE_NAMESPACE = '__speed_trap__';
 
   var SpeedTrap = {
-    init: function(options) {
+    init: function (options) {
       options = options || {};
-      this.navigationTiming = options.navigationTiming || navigationTiming;
+      this.navigationTiming = create(NavigationTiming);
+      this.navigationTiming.init(options);
 
       this.timers = create(Timers);
       this.timers.init();
 
       this.events = create(Events);
       this.events.init();
+
+      this.stored = create(PersistentStorage);
+      this.uuid = guid();
     },
 
-    get: function() {
+    get: function () {
       return {
-        navigationTiming: this.navigationTiming,
+        uuid: this.uuid,
+        navigationTiming: this.navigationTiming.get(),
         timers: this.timers.get(),
         events: this.events.get()
       };
+    },
+
+    store: function () {
+      this.stored.store(this.get());
+    }
+  };
+
+  var NAVIGATION_TIMING_FIELDS = {
+    'navigationStart': undefined,
+    'unloadEventStart': undefined,
+    'unloadEventEnd': undefined,
+    'redirectStart': undefined,
+    'redirectEnd': undefined,
+    'fetchStart': undefined,
+    'domainLookupStart': undefined,
+    'domainLookupEnd': undefined,
+    'connectStart': undefined,
+    'connectEnd': undefined,
+    'secureConnectionStart': undefined,
+    'requestStart': undefined,
+    'responseStart': undefined,
+    'responseEnd': undefined,
+    'domLoading': undefined,
+    'domInteractive': undefined,
+    'domContentLoadedEventStart': undefined,
+    'domContentLoadedEventEnd': undefined,
+    'domComplete': undefined,
+    'loadEventStart': undefined,
+    'loadEventEnd': undefined
+  };
+
+  var navigationTiming;
+  try {
+    navigationTiming = window.performance.timing;
+  } catch (e) {
+    navigationTiming = create(NAVIGATION_TIMING_FIELDS);
+  }
+
+  var NavigationTiming = {
+    init: function (options) {
+      options = options || {};
+      this.navigationTiming = options.navigationTiming || navigationTiming;
+    },
+
+    get: function() {
+      return this.navigationTiming;
     }
   };
 
   var Timers = {
-    init: function() {
+    init: function () {
       this.completed = {};
       this.running = {};
     },
 
-    start: function(name) {
+    start: function (name) {
       var start = now();
       if (this.running[name]) throw new Error(name + ' timer already started');
 
       this.running[name] = start;
     },
 
-    stop: function(name) {
+    stop: function (name) {
       var stop = now();
 
       if ( ! this.running[name]) throw new Error(name + ' timer not started');
@@ -64,40 +110,72 @@
       delete this.running[name];
     },
 
-    get: function(name) {
+    get: function (name) {
       if ( ! name) return this.completed;
       return this.completed[name];
     }
   };
 
   var Events = {
-    init: function() {
+    init: function () {
       this.events = [];
     },
 
-    capture: function(name) {
+    capture: function (name) {
       this.events.push({
         type: name,
         timestamp: now()
       });
     },
 
-    get: function() {
+    get: function () {
       return this.events;
     }
   };
 
+  var PersistentStorage = {
+    store: function (dataToStore) {
+      var storedData = this.get();
+      var lastItem = storedData[storedData.length - 1];
+
+      if (lastItem && lastItem.uuid === dataToStore.uuid) {
+        // this session is already stored, get rid of the old data.
+        storedData.pop();
+      }
+
+      storedData.push(dataToStore);
+      localStorage.setItem(LOCAL_STORAGE_NAMESPACE, JSON.stringify(storedData));
+    },
+
+    get: function () {
+      var stringified = localStorage.getItem(LOCAL_STORAGE_NAMESPACE) || '[]';
+      var data = JSON.parse(stringified);
+      return data;
+    },
+
+    clear: function () {
+      localStorage.removeItem(LOCAL_STORAGE_NAMESPACE);
+    }
+  };
+
   function create(proto) {
-    if (typeof proto === "function") return new proto();
     if (Object.create) return Object.create(proto);
 
-    var F = function() {};
+    var F = function () {};
     F.prototype = proto;
     return new F();
   }
 
   function now() {
     return new Date().getTime();
+  }
+
+  function guid() {
+    // from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
   }
 
   exports.SpeedTrap = create(SpeedTrap);
--- tests/spec/speed-trap_test.js
+++ tests/spec/speed-trap_test.js
@@ -7,46 +7,74 @@
   var assert = chai.assert;
   var SpeedTrap = window.SpeedTrap;
 
-  describe('Library', function() {
+  describe('Speed Trap', function() {
     var speedTrap;
 
     beforeEach(function() {
       speedTrap = Object.create(SpeedTrap);
       speedTrap.init({});
+      speedTrap.stored.clear();
     });
 
     it('exists', function() {
       assert.isObject(window.SpeedTrap);
     });
 
-    it('attaches to navigation timing automatically', function() {
-      assert.ok('loadEventEnd' in speedTrap.navigationTiming);
-    });
-
-    it('timers can be captured', function(done) {
-      speedTrap.timers.start('named_timer');
-      setTimeout(function() {
-        speedTrap.timers.stop('named_timer');
-        var timers = speedTrap.timers.get('named_timer');
-        assert.isArray(timers);
-        assert.ok(timers[0].elapsed >= 1);
-        done();
-      }, 1);
-    });
-
-    it('events can be captured', function() {
-      speedTrap.events.capture('event');
-      var events = speedTrap.events.get();
-      assert.isArray(events);
-      assert.equal(events[0].type, 'event');
-      assert.ok(events[0].timestamp);
-    });
-
     it('all data can be retreived', function() {
       var data = speedTrap.get();
-      assert.ok('navigationTiming' in data);
+      assert.isObject(data.navigationTiming);
       assert.isObject(data.timers);
       assert.isArray(data.events);
+    });
+
+    describe('navigationTiming', function() {
+      it('is captured', function() {
+        assert.ok('loadEventEnd' in speedTrap.navigationTiming.get());
+      });
+    });
+
+    describe('timers', function() {
+      it('can be captured', function(done) {
+        speedTrap.timers.start('named_timer');
+        setTimeout(function() {
+          speedTrap.timers.stop('named_timer');
+          var timers = speedTrap.timers.get('named_timer');
+          assert.isArray(timers);
+          assert.ok(timers[0].elapsed >= 1);
+          done();
+        }, 1);
+      });
+    });
+
+    describe('events', function() {
+      it('can be captured', function() {
+        speedTrap.events.capture('event');
+        var events = speedTrap.events.get();
+        assert.isArray(events);
+        assert.equal(events[0].type, 'event');
+        assert.ok(events[0].timestamp);
+      });
+    });
+
+    describe('stored', function() {
+      it('current session can be stored', function() {
+        speedTrap.store();
+        var sessions = speedTrap.stored.get();
+        assert.isArray(sessions);
+
+        var session = sessions[0];
+        assert.ok('navigationTiming' in session);
+        assert.isObject(session.timers);
+        assert.isArray(session.events);
+
+      });
+
+      it('a session is only stored once', function() {
+        speedTrap.store();
+        speedTrap.store();
+        var sessions = speedTrap.stored.get();
+        assert.equal(sessions.length, 1);
+      });
     });
   });
 
